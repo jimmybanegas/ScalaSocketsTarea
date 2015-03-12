7c20267a255cb113135ad0e9d01498a94b010c52
Jimmy Ramos <jimmy.ramos@unitec.edu>
2015-03-11 23:21:34 -0600

    hello

--- ScalaSocketsTarea/src/Hola.scala
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Jimmy Banegas on 11/03/2015.
- */
-class Hola {
-
-}
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by Jimmy Banegas on 11/03/2015.
+ */
+object Hello {
+  def main(args: Array[String]) {
+    println("¡Hola, Rafael Sequeiros, creo que me darás 100 lempiras pronto!")
+  }
+}
